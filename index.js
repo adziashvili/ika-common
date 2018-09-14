978b3b7bc418c184f7ab67ee8e38a1e889e2b3af
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-09-14 22:33:46 +0800

    1.0.4

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-// import {
-//   FSHelper,
-//   JSONHelper,
-//   StringHelper,
-//   StringBuffer,
-//   DateHelper,
-//   ReportHelper,
-//   ExcelReader,
-//   MathHelper
-// } from './src/helpers'
-
-import * as HELPERS from './src/helpers'
-
-export const StringHelper = HELPERS.StringHelper
-
-// module.export = {
-//   FSHelper,
-//   JSONHelper,
-//   StringHelper,
-//   StringBuffer,
-//   DateHelper,
-//   ReportHelper,
-//   ExcelReader,
-//   MathHelper
-// }
+export {
+  FSHelper,
+  JSONHelper,
+  StringHelper,
+  StringBuffer,
+  DateHelper,
+  ReportHelper,
+  ExcelReader,
+  MathHelper
+}
+from './src/helpers'
