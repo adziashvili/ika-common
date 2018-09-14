0dc36693c4690d70d0367b51693f2527d32f11ba
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-09-15 02:35:17 +0800

    Removed index.js from helpers folder.

--- source/index.js
+++ source/index.js
@@ -1,14 +1,11 @@
-import {
-  FSHelper,
-  JSONHelper,
-  StringHelper,
-  StringBuffer,
-  DateHelper,
-  ReportHelper,
-  ExcelReader,
-  MathHelper
-}
-from './helpers'
+import FSHelper from './helpers/fsHelper'
+import JSONHelper from './helpers/jsonHelper'
+import StringHelper from './helpers/stringHelper'
+import StringBuffer from './helpers/stringBuffer'
+import DateHelper from './helpers/dateHelper'
+import ReportHelper from './helpers/reportHelper'
+import ExcelReader from './helpers/excelReader'
+import MathHelper from './helpers/mathHelper'
 
 require('colors')
 
