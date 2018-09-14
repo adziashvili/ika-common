341749527616a4dc6df615ff6ff3fdfc49030e85
Adziashvili <itzik@186590d20f27.ant.amazon.com>
2018-09-14 22:23:45 +0800

    1.0.3

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-import {
-  FSHelper,
-  JSONHelper,
-  StringHelper,
-  StringBuffer,
-  DateHelper,
-  ReportHelper,
-  ExcelReader,
-  MathHelper
-} from './src/helpers'
+// import {
+//   FSHelper,
+//   JSONHelper,
+//   StringHelper,
+//   StringBuffer,
+//   DateHelper,
+//   ReportHelper,
+//   ExcelReader,
+//   MathHelper
+// } from './src/helpers'
 
-module.export = {
-  FSHelper,
-  JSONHelper,
-  StringHelper,
-  StringBuffer,
-  DateHelper,
-  ReportHelper,
-  ExcelReader,
-  MathHelper
-}
+import * as HELPERS from './src/helpers'
+
+export const StringHelper = HELPERS.StringHelper
+
+// module.export = {
+//   FSHelper,
+//   JSONHelper,
+//   StringHelper,
+//   StringBuffer,
+//   DateHelper,
+//   ReportHelper,
+//   ExcelReader,
+//   MathHelper
+// }
